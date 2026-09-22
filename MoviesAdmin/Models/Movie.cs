b84cb6295d154bb717870a94e1422dfd90138a59
Snipe <w0520600@nscc.ca>
2026-09-22 10:22:58 -0300

    builded the movie base model and home

---
+++ MoviesAdmin/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; } = string.Empty;// The Title of the movie
+
+        public string rating { get; set; } = string.Empty;// The rating for the movie like pg 13+
+
+        public string description { get; set; } = string.Empty;// A small description for the movie
+
+        public string genre { get; set; } = string.Empty;// The genre for the movie
+
+        public float runtime { get; set; }// Movie's runtime
+
+        public DateTime releases {  get; set; } = DateTime.Now;// The date the movie came out 
+
+
+    }
+}
